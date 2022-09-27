3eb5ba5a46b1c097b221b97f13bcb5e32a3ba3cb
Kacper Biaduń <kacperbiadun.12@gmail.com>
2022-09-28 00:31:00 +0200

    Initial Commit

---
+++ FlatMateOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatMateOrganizer
+//
+//  Created by kacper on 28/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
